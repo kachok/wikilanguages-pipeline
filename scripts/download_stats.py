0e14b44aefb01a508365216afa2dfb3434a36c8d
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2012-01-05 12:46:59 -0500

    added missing file for donwloading wikipedia stats (unused now)

--- scripts/download_stats.py
+++ scripts/download_stats.py
@@ -1,3 +1,5 @@
+# downloads wikipedia raw stats
+
 import urllib
 import re
 
@@ -7,9 +9,7 @@ import time
 data = urllib.urlopen('http://dumps.wikimedia.org/other/pagecounts-raw/2011/2011-11/').read()    
 #print data
 
-#datafiles name pattern - usagov_bitly_data2011-07-29-1311919454
 p = re.compile('pagecounts-\d{8}-\d{6}.gz')
-#print p.findall('<tr><td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td><td><a href="usagov_bitly_data2011-07-29-1311919454">usagov_bitly_data2011-07-29-1311919454</a></td><td align="right">29-Jul-2011 07:04  </td><td')
 
 m=p.findall(data)
 
